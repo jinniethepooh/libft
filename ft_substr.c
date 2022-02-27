c82f1ca7a2e6b18f88f6f9f51aa4a54972036712
Chetsupa Chetanachan <cchetana@z2t13c1.42bangkok.com>
2022-02-27 16:48:51 +0700

    revised substr malloc

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: cchetana <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/20 18:24:23 by cchetana          #+#    #+#             */
-/*   Updated: 2022/02/27 14:40:16 by cchetana         ###   ########.fr       */
+/*   Updated: 2022/02/27 16:41:23 by cchetana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,11 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 
 	if (!s)
 		return (NULL);
-	if (ft_strlen(s) <= start)
+	lenr = len;
+	if (ft_strlen(s) <= start || (int)len < 0)
 		lenr = 0;
-	else if (len > ft_strlen(s))
+	else if (len >= ft_strlen(s))
 		lenr = ft_strlen(s) - start;
-	else
-		lenr = len;
 	ss = (char *)malloc(lenr + 1);
 	if (ss == NULL)
 		return (NULL);
